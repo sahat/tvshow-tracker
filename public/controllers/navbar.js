41bb7461dc86873b0bf84c69f5b74bd2d55f7b1f
Sahat Yalkabov <sakhat@gmail.com>
2014-05-06 17:15:21 -0400

    Navbar controller cleanup

--- public/controllers/navbar.js
+++ public/controllers/navbar.js
@@ -1,30 +1,13 @@
 angular.module('MyApp')
-  .controller('NavbarCtrl', ['$scope', '$location', '$alert', 'Auth', function($scope, $location, $alert, Auth) {
-    console.log('navbar controller loaded!');
-
-    $scope.dropdown = [
-      {
-        text: "My Shows",
-        href: '#page2'
-      },
-      {
-        divider: true
-      },
-      {
-        text: 'Logout',
-        click: 'logout()'
-      }
-    ];
-
+  .controller('NavbarCtrl', ['$scope', '$alert', 'Auth', function($scope, $alert, Auth) {
     $scope.logout = function() {
-      Auth.logout()
-        .then(function() {
-          $alert({
-            content: 'You have been logged out.',
-            placement: 'top-right',
-            type: 'info',
-            duration: 3
-          });
+      Auth.logout().then(function() {
+        $alert({
+          content: 'You have been logged out.',
+          placement: 'top-right',
+          type: 'info',
+          duration: 3
         });
+      });
     };
   }]);
