be2a14d948eb49b1c00bca401e9bc75693869453
Sahat Yalkabov <sakhat@gmail.com>
2014-05-06 04:03:15 -0400

    Auth service refactoring and cleanup

--- public/services/auth.js
+++ public/services/auth.js
@@ -2,47 +2,30 @@ angular.module('MyApp')
   .factory('Auth', ['$http', '$location', '$rootScope', '$cookieStore',
     function($http, $location, $rootScope, $cookieStore) {
 
-    // Get currentUser from cookie
-    $rootScope.currentUser = $cookieStore.get('user') || null;
+    $rootScope.currentUser = $cookieStore.get('user');
     $cookieStore.remove('user');
 
     return {
-      login: function(user, callback) {
-        var cb = callback || angular.noop;
-
+      login: function(user) {
         return $http.post('/api/login', user).success(function(data) {
-          console.log(data);
           $rootScope.currentUser = data;
-          return cb();
         });
       },
 
-      logout: function(callback) {
-        var cb = callback || angular.noop;
+      signup: function(user) {
+        return $http.post('/api/signup', user).success(function() {
+          $rootScope.currentUser = data;
+        });
+      },
 
+      logout: function() {
         return $http.get('/api/logout').success(function() {
           $rootScope.currentUser = null;
-          return cb();
-        });
-      },
-
-      signup: function(user, callback) {
-        var cb = callback || angular.noop;
-
-        return $http.post('/api/signup', user).success(function(data) {
-          $rootScope.currentUser = data;
-          return cb();
         });
       },
 
       currentUser: function() {
         return $rootScope.currentUser;
-      },
-
-      isLoggedIn: function() {
-        var user = $rootScope.currentUser;
-        return !!user;
       }
-
     };
   }]);
