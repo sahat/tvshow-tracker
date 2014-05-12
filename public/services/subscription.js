bdaf92d69782190f591d66c47ba34c0738c845b6
Sahat Yalkabov <sakhat@gmail.com>
2014-05-12 03:36:48 -0400

    Subscription service refactoring

--- public/services/subscription.js
+++ public/services/subscription.js
@@ -1,13 +1,11 @@
 angular.module('MyApp')
-  .factory('Subscription', ['$http', '$rootScope', '$cookieStore', function($http, $rootScope, $cookieStore) {
+  .factory('Subscription', ['$http', function($http) {
     return {
       subscribe: function(show, user) {
-        return $http.post('/api/subscribe', { showId: show._id, userId: user._id }).success(function(data) {
-        });
+        return $http.post('/api/subscribe', { showId: show._id, userId: user._id });
       },
       unsubscribe: function(show, user) {
-        return $http.post('/api/unsubscribe', { showId: show._id, userId: user._id }).success(function(data) {
-        });
+        return $http.post('/api/unsubscribe', { showId: show._id, userId: user._id });
       }
     };
   }]);
