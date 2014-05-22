3f05c6000283155c4d5ce42dffb32c8af142616a
Sahat Yalkabov <sakhat@gmail.com>
2014-05-22 15:10:47 -0400

    Split angular config into 3 separate configs

--- public/app.js
+++ public/app.js
@@ -1,53 +1,51 @@
 angular.module('MyApp', ['ngCookies', 'ngResource', 'ngAnimate', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
-  .config(['$routeProvider', '$httpProvider', '$locationProvider',
-    function($routeProvider, $httpProvider, $locationProvider) {
-
-      $locationProvider.html5Mode(true);
-
-      $routeProvider
-        .when('/', {
-          templateUrl: 'views/home.html',
-          controller: 'MainCtrl',
-          title: 'Home Page'
-        })
-        .when('/shows/:id', {
-          controller: 'DetailCtrl',
-          templateUrl: 'views/detail.html'
-        })
-        .when('/login', {
-          templateUrl: 'views/login.html',
-          controller: 'LoginCtrl',
-          title: 'Lo gin'
-        })
-        .when('/signup', {
-          templateUrl: 'views/signup.html',
-          controller: 'SignupCtrl',
-          title: 'Create Account'
-        })
-        .when('/add', {
-          templateUrl: 'views/add.html',
-          controller: 'AddCtrl',
-          title: 'Add Show'
-        })
-        .otherwise({
-          redirectTo: '/'
-        });
-
-      // Intercept 401s and 403s and redirect you to login
-      $httpProvider.interceptors.push(['$q', '$location', function($q, $location) {
-        return {
-          responseError: function(response) {
-            if (response.status === 401 || response.status === 403) {
-              $location.path('#/login');
-              return $q.reject(response);
-            } else {
-              return $q.reject(response);
-            }
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html',
+        controller: 'MainCtrl',
+        title: 'Home Page'
+      })
+      .when('/shows/:id', {
+        controller: 'DetailCtrl',
+        templateUrl: 'views/detail.html'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        title: 'Lo gin'
+      })
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignupCtrl',
+        title: 'Create Account'
+      })
+      .when('/add', {
+        templateUrl: 'views/add.html',
+        controller: 'AddCtrl',
+        title: 'Add Show'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }])
+  .config(['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode(true);
+  }])
+  .config(['$httpProvider', function($httpProvider) {
+    $httpProvider.interceptors.push(['$q', '$location', function($q, $location) {
+      return {
+        responseError: function(response) {
+          if (response.status === 401 || response.status === 403) {
+            $location.path('#/login');
+            return $q.reject(response);
+          } else {
+            return $q.reject(response);
           }
-        };
-      }]);
-
-    }])
+        }
+      };
+    }]);
+  }])
   .run(['$rootScope', '$location', 'Auth', function($rootScope, $location, Auth) {
     // Redirect to login if route requires auth and you're not logged in
     $rootScope.$on('$routeChangeStart', function(event, next) {
