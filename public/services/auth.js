47111cb038f247fc5d7d9df6248265f1c8e68502
Sahat Yalkabov <sakhat@gmail.com>
2014-05-23 10:55:40 -0400

    Refactored auth service

--- public/services/auth.js
+++ public/services/auth.js
@@ -1,30 +1,66 @@
 angular.module('MyApp')
-  .factory('Auth', ['$http', '$location', '$rootScope', '$cookieStore',
-    function($http, $location, $rootScope, $cookieStore) {
+  .factory('Auth', ['$http', '$location', '$rootScope', '$cookieStore', '$alert',
+    function($http, $location, $rootScope, $cookieStore, $alert) {
 
-    $rootScope.currentUser = $cookieStore.get('user');
-    $cookieStore.remove('user');
+      $rootScope.currentUser = $cookieStore.get('user');
+      $cookieStore.remove('user');
 
-      console.log($rootScope.currentUser);
+      return {
+        isLoggedIn: function() {
+          return $rootScope.currentUser ? true : false;
+        },
+        login: function(user) {
+          return $http.post('/api/login', user)
+            .success(function(response) {
+              $rootScope.currentUser = data;
+              $location.path('/');
 
-    return {
-      login: function(user) {
-        return $http.post('/api/login', user).success(function(data) {
-          $rootScope.currentUser = data;
-        });
-      },
+              $alert({
+                title: 'Cheers!',
+                content: 'You have successfully logged in.',
+                placement: 'top-right',
+                type: 'success',
+                duration: 3
+              });
+            })
+            .error(function(response) {
+              $alert({
+                title: 'Error!',
+                content: 'Invalid username or password.',
+                placement: 'top-right',
+                type: 'danger',
+                duration: 3
+              });
+            });
+        },
+        signup: function(user) {
+          return $http.post('/api/signup', user)
+            .success(function(data) {
+              $rootScope.currentUser = data;
+              $location.path('/');
 
-      signup: function(user) {
-        return $http.post('/api/signup', user).success(function(data) {
-          console.log(data);
-          $rootScope.currentUser = data;
-        });
-      },
-
-      logout: function() {
-        return $http.get('/api/logout').success(function() {
-          $rootScope.currentUser = null;
-        });
-      }
-    };
-  }]);
+              $alert({
+                title: 'Congratulations!',
+                content: 'Your account has been created.',
+                placement: 'top-right',
+                type: 'success',
+                duration: 3
+              });
+            })
+            .error(function(response) {
+              $alert({
+                title: 'Error!',
+                content: response.data,
+                placement: 'top-right',
+                type: 'danger',
+                duration: 3
+              });
+            });
+        },
+        logout: function() {
+          return $http.get('/api/logout').success(function() {
+            $rootScope.currentUser = null;
+          });
+        }
+      };
+    }]);
