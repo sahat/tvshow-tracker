3af4e6d116579b99d5e346caf65b706c99b93a95
Sahat Yalkabov <sakhat@gmail.com>
2014-05-23 10:48:34 -0400

    Removed unused module depdencies, moved alerts to Auth service

--- public/controllers/login.js
+++ public/controllers/login.js
@@ -1,25 +1,9 @@
 angular.module('MyApp')
-  .controller('LoginCtrl', ['$rootScope', '$scope', '$alert', '$location', '$window', 'Auth', function($rootScope, $scope, $alert, $location, $window, Auth) {
+  .controller('LoginCtrl', ['$scope', 'Auth', function($scope, Auth) {
     $scope.login = function() {
-      Auth.login({ email: $scope.email, password: $scope.password })
-        .then(function() {
-          $location.path('/');
-          $alert({
-            title: 'Cheers!',
-            content: 'You have successfully logged in.',
-            placement: 'top-right',
-            type: 'success',
-            duration: 3
-          });
-        })
-        .catch(function(response) {
-          $alert({
-            title: 'Error!',
-            content: 'Invalid username or password.',
-            placement: 'top-right',
-            type: 'danger',
-            duration: 3
-          });
-        });
+      Auth.login({
+        email: $scope.email,
+        password: $scope.password
+      });
     };
   }]);
--- public/controllers/signup.js
+++ public/controllers/signup.js
@@ -1,25 +1,9 @@
 angular.module('MyApp')
-  .controller('SignupCtrl', ['$rootScope', '$scope', '$alert', '$location', '$window', 'Auth', function($rootScope, $scope, $alert, $location, $window, Auth) {
+  .controller('SignupCtrl', ['$scope', 'Auth', function($scope, Auth) {
     $scope.signup = function() {
-      Auth.signup({ email: $scope.email, password: $scope.password })
-        .then(function(data) {
-          $location.path('/');
-          $alert({
-            title: 'Congratulations!',
-            content: 'Your account has been created.',
-            placement: 'top-right',
-            type: 'success',
-            duration: 3
-          });
-        })
-        .catch(function(response) {
-          $alert({
-            title: 'Error!',
-            content: response.data,
-            placement: 'top-right',
-            type: 'danger',
-            duration: 3
-          });
-        });
+      Auth.signup({
+        email: $scope.email,
+        password: $scope.password
+      });
     };
   }]);
