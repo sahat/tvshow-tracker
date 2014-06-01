ae128a7d8bd237a42ad1b7828ec2855c45244677
Sahat Yalkabov <sakhat@gmail.com>
2014-06-01 16:28:34 -0400

    Removed titles from routes

--- public/app.js
+++ public/app.js
@@ -5,27 +5,23 @@ angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mg
     $routeProvider
       .when('/', {
         templateUrl: 'views/home.html',
-        controller: 'MainCtrl',
-        title: 'Home Page'
+        controller: 'MainCtrl'
       })
       .when('/shows/:id', {
-        controller: 'DetailCtrl',
-        templateUrl: 'views/detail.html'
+        templateUrl: 'views/detail.html',
+        controller: 'DetailCtrl'
       })
       .when('/login', {
         templateUrl: 'views/login.html',
-        controller: 'LoginCtrl',
-        title: 'Login'
+        controller: 'LoginCtrl'
       })
       .when('/signup', {
         templateUrl: 'views/signup.html',
-        controller: 'SignupCtrl',
-        title: 'Create Account'
+        controller: 'SignupCtrl'
       })
       .when('/add', {
         templateUrl: 'views/add.html',
-        controller: 'AddCtrl',
-        title: 'Add Show'
+        controller: 'AddCtrl'
       })
       .otherwise({
         redirectTo: '/'
