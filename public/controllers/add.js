4e113889909f1ccdefa4f962488ca0d3c640ab1c
Sahat Yalkabov <sakhat@gmail.com>
2014-06-07 16:44:14 -0400

    Refactored AddCtrl

--- public/controllers/add.js
+++ public/controllers/add.js
@@ -1,15 +1,14 @@
 angular.module('MyApp')
-  .controller('AddCtrl', ['$scope', '$alert', '$http', function($scope, $alert, $http) {
+  .controller('AddCtrl', ['$scope', '$alert', 'Show', function($scope, $alert, Show) {
     $scope.addShow = function() {
-      $http.post('/api/shows', { showName: $scope.showName })
-        .success(function() {
-          $scope.showName = '';
-          $alert({
-            content: 'TV show has been added.',
-            placement: 'top-right',
-            type: 'success',
-            duration: 3
-          });
-        })
+      Show.save({ showName: $scope.showName }, function() {
+        $scope.showName = '';
+        $alert({
+          content: 'TV show has been added.',
+          placement: 'top-right',
+          type: 'success',
+          duration: 3
+        });
+      });
     };
   }]);
