da64696521de838c1264395d757d4fa144a4dbb9
Sahat Yalkabov <sakhat@gmail.com>
2014-06-17 20:25:55 -0400

    Fixed alignment

--- public/filters/fromNow.js
+++ public/filters/fromNow.js
@@ -1,6 +1,6 @@
 angular.module('MyApp').
   filter('fromNow', function() {
-  return function(date) {
-    return moment(date).fromNow();
-  }
-});
+    return function(date) {
+      return moment(date).fromNow();
+    }
+  });
