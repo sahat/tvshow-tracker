c5bcf7c91992f1dd5976941913c0162657b5c7db
Sahat Yalkabov <sakhat@gmail.com>
2014-08-02 01:55:39 -0400

    code cleanup

--- public/controllers/login.js
+++ public/controllers/login.js
@@ -1,19 +1,13 @@
 angular.module('MyApp')
   .controller('LoginCtrl', function($scope, Auth) {
     $scope.login = function() {
-      Auth.login({
-        email: $scope.email,
-        password: $scope.password
-      });
+      Auth.login({ email: $scope.email, password: $scope.password });
     };
-
     $scope.facebookLogin = function() {
       Auth.facebookLogin();
     };
-
     $scope.googleLogin = function() {
       Auth.googleLogin();
     };
-
     $scope.pageClass = 'fadeZoom';
   });
