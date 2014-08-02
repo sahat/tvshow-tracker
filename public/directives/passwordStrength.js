5e5d432233979d966a0c7921a8783918c8c4b5f3
Sahat Yalkabov <sakhat@gmail.com>
2014-08-02 01:18:00 -0400

    added if-statement to check if $modelValue is present

--- public/directives/passwordStrength.js
+++ public/directives/passwordStrength.js
@@ -15,14 +15,16 @@ angular.module('MyApp')
 
         element.bind('keyup', function() {
           angular.forEach(dots, function(el) { el.style.backgroundColor = '#ebeef1'; });
-          if (ngModel.$modelValue.length > 8) {
-            angular.forEach(strongest, function(el) { el.style.backgroundColor = '#008cdd'; });
-          } else if (ngModel.$modelValue.length > 5) {
-            angular.forEach(strong, function(el) { el.style.backgroundColor = '#6ead09'; });
-          } else if (ngModel.$modelValue.length > 3) {
-            angular.forEach(weak, function(el) { el.style.backgroundColor = '#e09115'; });
-          } else {
-            weakest.style.backgroundColor = '#e01414';
+          if (ngModel.$modelValue) {
+            if (ngModel.$modelValue.length > 8) {
+              angular.forEach(strongest, function(el) { el.style.backgroundColor = '#008cdd'; });
+            } else if (ngModel.$modelValue.length > 5) {
+              angular.forEach(strong, function(el) { el.style.backgroundColor = '#6ead09'; });
+            } else if (ngModel.$modelValue.length > 3) {
+              angular.forEach(weak, function(el) { el.style.backgroundColor = '#e09115'; });
+            } else {
+              weakest.style.backgroundColor = '#e01414';
+            }
           }
         });
       },
